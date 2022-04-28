cc205b61e8872a005f1db86b6761b905e8ff5769
Igor.tdf <104040266+IgorTDF@users.noreply.github.com>
2022-04-28 16:31:58 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+let vueltacompletas :number= 4;
+
+let tiempodevuelta : number= 2'minuto'
+
+let tiempototal : number= vueltacompletas * tiempodevuelta
+
+console.log ('tiempo total' tiempototal 'minutos')
+console.log ('promedio de vuelta' vueltacompletas/tiempototal 'segundos')
